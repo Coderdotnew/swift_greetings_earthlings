8d80a9bb4cd17930b01a2779e2f776ce623d91e9
Stephanholly <sholly@usc.edu>
2016-10-19 09:37:59 -0700

    coding challenge 1

---
+++ greetings_earthlings.swift
@@ -0,0 +1,10 @@
+// enter your code inside each function below
+
+
+// 1. create a function called "greetings" that prints out the string: "Greetings Earthlings! We come in peace!".
+
+// 2. create a function called "greetingsWithReturn" that returns the string: "Greetings Earthlings! We come in peace!".
+
+// 3. create a function called "warning" that prints out the string: "Just kidding. We only come in peace if you know how to code!".
+
+// 3. create a function called "warningWithReturn" that returns the string: "Just kidding. We only come in peace if you know how to code!".
